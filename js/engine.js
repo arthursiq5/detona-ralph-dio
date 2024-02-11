1f6edc783176a4e16e6d91b307ffc2b91380190b
arthursiq5 <arthur.siq5@gmail.com>
2024-02-11 18:30:30 -0300

    Adicionados sons e finalização do jogo

--- js/engine.js
+++ js/engine.js
@@ -6,11 +6,39 @@ const state = {
         score: document.getElementById('score'),
     },
     values: {
-        timerId: null,
         gameVelocity: 1000,
         hitPosition: null,
         result: 0,
+        currentTime: 60,
     },
+    actions: {
+        timerId: setInterval(randomSquare, 1000),
+        countDownTimerId: setInterval(countDown, 1000),
+    },
+}
+
+const sounds = {
+    defeat: 'defeat.wav',
+    hit: 'hit.wav',
+    miss: 'miss.ogg',
+    victoryCry: 'victory-cry.wav',
+    victory: 'victory.mp3',
+}
+
+function countDown() {
+    state.values.currentTime--;
+    state.view.timeLeft.textContent = state.values.currentTime
+    if (state.values.currentTime <= 0) {
+        clearInterval(state.actions.timerId)
+        clearInterval(state.actions.countDownTimerId)
+        alert(`Game Over! O seu resultado foi: ${state.values.result}`)
+    }
+}
+
+function playSound(audioName, volume=0.2) {
+    let audio = new Audio(`../assets/audios/${audioName}`)
+    audio.volume = 0.2
+    audio.play()
 }
 
 function randomSquare () {
@@ -23,10 +51,14 @@ function randomSquare () {
 }
 
 function moveEnemy() {
-    state.values.timerId = setInterval(
+    state.actions.timerId = setInterval(
         randomSquare,
         state.values.gameVelocity
     )
+}
+
+function loseLife() {
+
 }
 
 function addListenerHitbox () {
@@ -36,14 +68,16 @@ function addListenerHitbox () {
                 state.values.result += 1
                 state.view.score.textContent = state.values.result
                 state.values.hitPosition = null
+                playSound(sounds.hit)
+                return;
             }
+            playSound(sounds.miss)
         })
     })
 }
 
 function initialize () {
     addListenerHitbox()
-    moveEnemy()
 }
 
 initialize()
